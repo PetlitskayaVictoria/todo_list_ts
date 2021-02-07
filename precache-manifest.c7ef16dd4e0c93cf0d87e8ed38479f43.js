159c186e81fdf0aefa7d3591df2e4727163bdd50
PetlitskayaVictoria <petlitskayavictoria@gmail.com>
2021-02-07 18:34:15 +0300

    Updates

---
+++ precache-manifest.c7ef16dd4e0c93cf0d87e8ed38479f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19023fceebc203f51382634cbf5eefd4",
+    "url": "/todo_list_ts/index.html"
+  },
+  {
+    "revision": "7a63fa7417d8cbf57915",
+    "url": "/todo_list_ts/static/css/main.9f3337f3.chunk.css"
+  },
+  {
+    "revision": "602c05fbc87dee81585a",
+    "url": "/todo_list_ts/static/js/2.ab3a3c22.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo_list_ts/static/js/2.ab3a3c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a63fa7417d8cbf57915",
+    "url": "/todo_list_ts/static/js/main.c4743963.chunk.js"
+  },
+  {
+    "revision": "17ec27a2a6904d46340d",
+    "url": "/todo_list_ts/static/js/runtime-main.5420f130.js"
+  }
+]);
